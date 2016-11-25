6f4a8541faafcdb449246593c4408453c63633df
likedo <francescoguidotti@likedo.it>
2016-11-25 18:21:24 +0100

    Add example commission junction

---
+++ examples/commissionJunction.php
@@ -0,0 +1,64 @@
+<?php
+use Padosoft\AffiliateNetwork\Networks\CommissionJunction;
+
+// Include config
+include_once 'config.php';
+
+$CommissionJunction = new CommissionJunction($_ENV['COMMISSIONJUNCTION_USERNAME'], $_ENV['COMMISSIONJUNCTION_PASSWORD'], $_ENV['COMMISSIONJUNCTION_PASSWORD_API'], $_ENV['COMMISSIONJUNCTION_WEBSITEID']);
+$isLogged = $CommissionJunction->checkLogin();
+if($isLogged) {
+
+    /**
+     * Merchants List
+     */
+
+    echo '<h1>Merchants list</h1>';
+    $merchantList = $CommissionJunction->getMerchants();
+    echo '<pre>';
+    var_dump($merchantList);
+    echo '</pre>';
+
+    /**
+     *Sales list
+     */
+
+    echo '<h1>Sales</h1>';
+    $merchantList = array(
+        '11078' => array('cid' => '11078', 'name' => 'Yeppon IT'),
+        '2725' => array('cid' => '2725', 'name' => 'Meridiana IT')
+    );
+    $sales = $CommissionJunction->getSales(new DateTime('2015-10-17'), new DateTime('2016-11-15'), $merchantList);
+    echo '<pre>';
+    var_dump($sales);
+    echo '</pre>';
+
+
+    /**
+     * Stats list
+     */
+
+    /*
+    echo '<h1>Stats</h1>';
+    $stats = $CommissionJunction->getStats(new DateTime('2016-10-14'), new DateTime('2016-11-15'));
+    echo '<pre>';
+    var_dump($stats);
+    echo '</pre>';
+    */
+
+    /**
+     * Deals
+     */
+
+    echo '<h1>Deals</h1>';
+    $deals = $CommissionJunction->getDeals();
+    echo '<pre>';
+    var_dump($deals);
+    echo '</pre>';
+
+    echo '<h1>Single deal merchant id = 3857130</h1>';
+    $deals = $CommissionJunction->getDeals(3857130);
+    echo '<pre>';
+    var_dump($deals);
+    echo '</pre>';
+
+}
---
+++ src/Networks/CommissionJunction.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Padosoft\AffiliateNetwork\Networks;
+
+use Padosoft\AffiliateNetwork\Transaction;
+use Padosoft\AffiliateNetwork\Merchant;
+use Padosoft\AffiliateNetwork\Stat;
+use Padosoft\AffiliateNetwork\Deal;
+use Padosoft\AffiliateNetwork\AbstractNetwork;
+use Padosoft\AffiliateNetwork\NetworkInterface;
+
+// require "../vendor/fubralimited/php-oara/Oara/Network/Publisher/CommissionJunction/Zapi/ApiClient.php";
+
+/**
+ * Class CommissionJunction
+ * @package Padosoft\AffiliateNetwork\Networks
+ */
+class CommissionJunction extends AbstractNetwork implements NetworkInterface
+{
+    /**
+     * @var object
+     */
+    private $_network = null;
+    // private $_apiClient = null;
+    private $_username = '';
+    private $_password = '';
+    private $_passwordApi = '';
+    private $_website_id = '';
+
+    /**
+     * @method __construct
+     */
+    public function __construct(string $username, string $password, string $passwordApi, string $website_id)
+    {
+        $this->_network = new \Oara\Network\Publisher\CommissionJunction;
+        $this->_username = $username;
+        $this->_password = $password;
+        $this->_passwordApi = $passwordApi;
+        $this->_website_id = $website_id;
+        // $this->_apiClient = \ApiClient::factory(PROTOCOL_JSON);
+    }
+
+    /**
+     * @return bool
+     */
+    public function checkLogin() : bool
+    {
+        $credentials = array();
+        $credentials["user"] = $this->_username;
+        $credentials["password"] = $this->_password;
+        $credentials["apipassword"] = $this->_passwordApi;
+
+        $this->_network->login($credentials);
+        if ($this->_network->checkConnection()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return array of Merchants
+     */
+    public function getMerchants() : array
+    {
+        $arrResult = array();
+        $merchantList = $this->_network->getMerchantList();
+        foreach($merchantList as $merchant) {
+            $Merchant = Merchant::createInstance();
+            $Merchant->merchant_ID = $merchant['cid'];
+            $Merchant->name = $merchant['name'];
+            $arrResult[] = $Merchant;
+        }
+
+        return $arrResult;
+    }
+
+    /**
+     * @param int $merchantID
+     * @return array of Deal
+     */
+    public function getDeals(int $merchantID = 0) : array
+    {
+        $response = $this->_apiCall('https://link-search.api.cj.com/v2/link-search?website-id='.$this->_website_id.'&promotion-type=coupon&advertiser-ids=joined');
+        if (\preg_match("/error/", $response)) {
+            return false;
+        }
+        $arrResult = array();
+        $arrResponse = xml2array($response);
+        if(!is_array($arrResponse) || count($arrResponse) <= 0) {
+            return $arrResult;
+        }
+        $arrCoupon = $arrResponse['cj-api']['links']['link'];
+        foreach($arrCoupon as $coupon) {
+            $Deal = Deal::createInstance();
+            $Deal->merchant_ID = $coupon['advertiser-id'];
+            $Deal->merchant_name = $coupon['advertiser-name'];
+            $Deal->ppc = $coupon['click-commission'];
+            $Deal->description = $coupon['description'];
+            $startDate = new \DateTime($coupon['promotion-start-date']);
+            $Deal->startDate = $startDate;
+            $startDate = new \DateTime($coupon['promotion-end-date']);
+            $Deal->endDate = $endDate;
+            $Deal->code = $coupon['coupon-code'];
+            if($merchantID > 0) {
+                if($merchantID == $coupon['advertiser-id']) {
+                    $arrResult[] = $Deal;
+                }
+            }
+            else {
+                $arrResult[] = $Deal;
+            }
+        }
+
+        return $arrResult;
+    }
+
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $merchantID
+     * @return array of Transaction
+     */
+    public function getSales(\DateTime $dateFrom, \DateTime $dateTo, array $arrMerchantID = array()) : array
+    {
+        $arrResult = array();
+        $transcationList = $this->_network->getTransactionList($arrMerchantID, $dateTo, $dateFrom);
+        foreach($transcationList as $transaction) {
+            $Transaction = Transaction::createInstance();
+            $Transaction->status = $transaction['status'];
+            $Transaction->amount = $transaction['amount'];
+            $Transaction->custom_ID = $transaction['custom_id'];
+            $Transaction->unique_ID = $transaction['unique_id'];
+            $Transaction->commission = $transaction['commission'];
+            $date = new \DateTime($transaction['date']);
+            $Transaction->date = $date; // $date->format('Y-m-d H:i:s');
+            $Transaction->merchant_ID = $transaction['merchantId'];
+            $arrResult[] = $Transaction;
+        }
+
+        return $arrResult;
+    }
+
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $merchantID
+     * @return array of Stat
+     */
+    public function getStats(\DateTime $dateFrom, \DateTime $dateTo, int $merchantID = 0) : array
+    {
+        return array();
+        /*
+        $this->_apiClient->setConnectId($this->_username);
+        $this->_apiClient->setSecretKey($this->_password);
+        $dateFromIsoEngFormat = $dateFrom->format('Y-m-d');
+        $dateToIsoEngFormat = $dateTo->format('Y-m-d');
+        $response = $this->_apiClient->getReportBasic($dateFromIsoEngFormat, $dateToIsoEngFormat);
+        $arrResponse = json_decode($response, true);
+        $reportItems = $arrResponse['reportItems'];
+        $Stat = Stat::createInstance();
+        $Stat->reportItems = $reportItems;
+
+        return array($Stat);
+        */
+    }
+
+    /**
+    * Api call CommissionJunction
+    */
+    private function _apiCall($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: " . $this->_passwordApi));
+        $curl_results = curl_exec($ch);
+        curl_close($ch);
+        return $curl_results;
+    }
+
+}
