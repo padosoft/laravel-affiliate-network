df62ea80b1938626c83b797ebf84803e95b8cbf2
Jessica Capuozzo <jessica.capuozzo@bravosconto.it>
2019-10-28 12:28:11 +0100

    Added new Network for TradeDoubler Whitelabels (TradeDoublerWhitelabel.php) + update package padosoft/php-oara v. 2.5.56

---
+++ src/Networks/TradeDoublerWhitelabel.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace Padosoft\AffiliateNetwork\Networks;
+
+use Padosoft\AffiliateNetwork\Transaction;
+use Padosoft\AffiliateNetwork\DealsResultset;
+use Padosoft\AffiliateNetwork\Merchant;
+use Padosoft\AffiliateNetwork\Stat;
+use Padosoft\AffiliateNetwork\Deal;
+use Padosoft\AffiliateNetwork\AbstractNetwork;
+use Padosoft\AffiliateNetwork\NetworkInterface;
+use Padosoft\AffiliateNetwork\TradeDoublerEx;
+use Padosoft\AffiliateNetwork\ProductsResultset;
+
+if (!defined('COOKIES_BASE_DIR')){
+    define('COOKIES_BASE_DIR',public_path('upload/report'));
+}
+/**
+ * Class TradeDoubler
+ * @package Padosoft\AffiliateNetwork\Networks
+ */
+class TradeDoublerWhitelabel extends AbstractNetwork implements NetworkInterface
+{
+    /**
+     * @var object
+     */
+    private $_network = null;
+    private $_apiClient = null;
+    private $_username = '';
+    private $_password = '';
+    private $_idSite = '';
+    private $_logged    = false;
+    protected $_tracking_parameter    = 'epi';
+
+    /**
+     * TradeDoubler constructor.
+     * @param string $username
+     * @param string $password
+     * @param string $idSite
+     */
+    public function __construct(string $username, string $password, string $idSite = '')
+    {
+        $this->_network = new TradeDoublerEx;
+        $this->_username = $username;
+        $this->_password = $password;
+        $this->_idSite = $idSite;
+        $this->_apiClient = null;
+        $this->login( $this->_username, $this->_password );
+    }
+
+    public function login(string $username, string $password, string $idSite = ''): bool
+    {
+        $this->_logged = false;
+        if (isNullOrEmpty( $username ) || isNullOrEmpty( $password )) {
+
+            return false;
+        }
+        $this->_username = $username;
+        $this->_password = $password;
+        $this->_idSite = $idSite;
+        $credentials = array();
+        $credentials["user"] = $this->_username;
+        $credentials["password"] = $this->_password;
+        $credentials["idSite"] = $this->_idSite;
+        $this->_network->login( $credentials );
+
+        if ($this->_network->checkConnection()) {
+            $this->_logged = true;
+
+        }
+
+        return $this->_logged;
+    }
+
+    /**
+     * @return bool
+     */
+    public function checkLogin() : bool
+    {
+        return $this->_logged;
+    }
+
+    /**
+     * @return array of Merchants
+     */
+    public function getMerchants() : array
+    {
+        if (!$this->checkLogin()) {
+            return array();
+        }
+        $arrResult = array();
+        $merchantList = $this->_network->getMerchantList();
+        foreach($merchantList as $merchant) {
+            $Merchant = Merchant::createInstance();
+            $Merchant->merchant_ID = $merchant['cid'];
+            $Merchant->name = $merchant['name'];
+            $arrResult[] = $Merchant;
+        }
+
+        return $arrResult;
+    }
+
+    /**
+     * @param int|null $merchantID
+     * @param int $page
+     * @param int $items_per_page
+     *
+     * @return DealsResultset
+     */
+    public function getDeals($merchantID,int $page=0,int $items_per_page=10) : DealsResultset
+    {
+        if (!isIntegerPositive($items_per_page)){
+            $items_per_page=10;
+        }
+        $result=DealsResultset::createInstance();
+        if (!$this->checkLogin()) {
+            return $result;
+        }
+        $arrResult = array();
+        $jsonVouchers = file_get_contents("https://api.tradedoubler.com/1.0/vouchers.json;dateOutputFormat=iso8601?token=".$_ENV['TRADEDOUBLER_TOKEN']);
+        $arrVouchers = json_decode($jsonVouchers, true);
+
+        foreach($arrVouchers as $voucher) {
+            $Deal = Deal::createInstance();
+            $Deal->setValues($voucher, [
+                'id' => 'deal_ID' ,
+                'programId' => 'merchant_ID' ,
+                'code' => 'code' ,
+                'updateDate' => 'update_date' ,
+                'publishStartDate' => 'publish_start_date' ,
+                'publishEndDate' => 'publish_end_date' ,
+                'startDate' => 'start_date' ,
+                'endDate' => 'end_date' ,
+                'title' => 'name' ,
+                'shortDescription' => 'short_description' ,
+                'description' => 'description' ,
+                'voucherTypeId' => 'deal_type' ,
+                'defaultTrackUri' => 'default_track_uri' ,
+                'landingUrl' => 'landing_url' ,
+                'discountAmount' => 'discount_amount' ,
+                'isPercentage' => 'is_percentage' ,
+                'publisherInformation' => 'information' ,
+                'languageId' => 'language' ,
+                'exclusive' => 'is_exclusive' ,
+                'siteSpecific' => 'is_site_specific' ,
+                'currencyId' => 'currency_initial' ,
+                'logoPath' => 'logo_path' ,
+            ]);
+            switch ($voucher['voucherTypeId']) {
+                case 1:
+                    $Deal->deal_type = \Oara\Utilities::OFFER_TYPE_VOUCHER;
+                    break;
+                case 2:
+                    $Deal->deal_type = \Oara\Utilities::OFFER_TYPE_DISCOUNT;
+                    break;
+                case 3:
+                    $Deal->deal_type = \Oara\Utilities::OFFER_TYPE_FREE_ARTICLE;
+                    break;
+                case 4:
+                    $Deal->deal_type = \Oara\Utilities::OFFER_TYPE_FREE_SHIPPING;
+                    break;
+                case 5:
+                    $Deal->deal_type = \Oara\Utilities::OFFER_TYPE_LOTTERY;
+                    break;
+            }
+
+            if($merchantID > 0) {
+                if($voucher['programId'] == $merchantID) {
+                    $arrResult[] = $Deal;
+                }
+            }
+            else {
+                $arrResult[] = $Deal;
+            }
+        }
+        $result->deals[]=$arrResult;
+        return $result;
+    }
+
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $merchantID
+     * @return array of Transaction
+     */
+    public function getSales(\DateTime $dateFrom, \DateTime $dateTo, array $arrMerchantID = array()) : array
+    {
+        if (!$this->checkLogin()) {
+            return array();
+        }
+        $arrResult = array();
+        if (count( $arrMerchantID ) < 1) {
+            $merchants = $this->getMerchants();
+            foreach ($merchants as $merchant) {
+                $arrMerchantID[$merchant->merchant_ID] = ['cid' => $merchant->merchant_ID, 'name' => $merchant->name];
+            }
+        }
+        $transactionList = $this->_network->getTransactionList($arrMerchantID, $dateFrom, $dateTo);
+        foreach($transactionList as $transaction) {
+            $Transaction = Transaction::createInstance();
+            $Transaction->merchant_ID = $transaction['merchantId'];
+            $date = new \DateTime($transaction['date']);
+            $Transaction->date = $date; // $date->format('Y-m-d H:i:s');
+            $Transaction->unique_ID = $transaction['unique_id'];
+            $Transaction->transaction_ID = $transaction['unique_id'] . '-' . $transaction['event_id'];
+            array_key_exists_safe( $transaction,
+                'custom_id' ) ? $Transaction->custom_ID = $transaction['custom_id'] : $Transaction->custom_ID = '';
+            $Transaction->status = $transaction['status'];
+            $Transaction->amount = $transaction['amount'];
+            $Transaction->commission = $transaction['commission'];
+            $Transaction->currency = $transaction['currency'];
+            $arrResult[] = $Transaction;
+        }
+
+        return $arrResult;
+    }
+
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $merchantID
+     * @return array of Stat
+     */
+    public function getStats(\DateTime $dateFrom, \DateTime $dateTo, int $merchantID = 0) : array
+    {
+        return array();
+    }
+
+
+    /**
+     * @param  array $params
+     *
+     * @return ProductsResultset
+     */
+    public function getProducts(array $params = []): ProductsResultset
+    {
+        // TODO: Implement getProducts() method.
+        throw new \Exception("Not implemented yet");
+    }
+
+    public function getTrackingParameter(){
+        return $this->_tracking_parameter;
+    }
+}
---
+++ src/TradeDoublerEx.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * Copyright (c) Padosoft.com 2017.
+ */
+namespace Padosoft\AffiliateNetwork;
+
+use Oara\Network\Publisher\TradeDoublerWhitelabel;
+
+class TradeDoublerEx extends TradeDoublerWhitelabel
+{
+    public function getTransactionList($merchantList = null, \DateTime $dStartDate = null, \DateTime $dEndDate = null)
+    {
+        $updateTransactions = Array();
+        $totalTransactions = Array();
+
+        if (isset($_ENV['TRADEDOUBLER_CURRENCY'])) {
+            $currency = $_ENV['TRADEDOUBLER_CURRENCY'];
+        }
+        else {
+            $currency = 'EUR';
+        }
+
+        // Get updated transactions in date range
+        $updatedTransactions = self::getTransactionListByDateType($merchantList, $dStartDate, $dEndDate, true, $currency);
+        // Get new transactions in date range
+        $insertedTransactions= self::getTransactionListByDateType($merchantList, $dStartDate, $dEndDate, false, $currency);
+
+        $parsedIDs=[];
+        foreach ($insertedTransactions as $transaction){
+            if (array_key_exists('unique_id', $transaction)){
+                $found=false;
+                for ($i=0;$i<count($totalTransactions);$i++){
+                    // Check also event_id to obtain a real unique key - <PN> - 2017-07-03
+                    if ($totalTransactions[$i]['unique_id']==$transaction['unique_id'] && $totalTransactions[$i]['event_id']==$transaction['event_id']){
+                        if ($totalTransactions[$i]['commission']<=0){
+                            $totalTransactions[$i] = $transaction;
+                            $found=true;
+                            break;
+                        }
+                        if ($totalTransactions[$i]['status']==\Oara\Utilities::STATUS_DECLINED){
+                            $totalTransactions[$i] = $transaction;
+                            $found=true;
+                            break;
+                        }
+                    }
+                }
+                if (!$found){
+                    $totalTransactions[] = $transaction;
+                }
+            }
+        }
+        foreach ($updatedTransactions as $transaction){
+            if (array_key_exists('unique_id',$transaction)){
+                $found=false;
+                for ($i=0;$i<count($totalTransactions);$i++){
+                    // Check also event_id to obtain a real unique key - <PN> - 2017-07-03
+                    if ($totalTransactions[$i]['unique_id']==$transaction['unique_id'] && $totalTransactions[$i]['event_id']==$transaction['event_id']){
+                        if ($totalTransactions[$i]['commission']<=0){
+                            $totalTransactions[$i]=$transaction;
+                            $found=true;
+                            break;
+                        }
+                        if ($totalTransactions[$i]['status']==\Oara\Utilities::STATUS_DECLINED){
+                            $totalTransactions[$i]=$transaction;
+                            $found=true;
+                            break;
+                        }
+
+                    }
+                }
+                if (!$found){
+                    $totalTransactions[]=$transaction;
+                }
+            }
+        }
+        return $totalTransactions;
+    }
+
+    public function getTransactionListByDateType($merchantList = null, \DateTime $dStartDate = null, \DateTime $dEndDate = null, bool $byDateUpdated = false, $currency = null)
+    {
+        $transactions = Array();
+
+        $valuesFormExport = array(new \Oara\Curl\Parameter('reportName', 'aAffiliateEventBreakdownReport'),
+            new \Oara\Curl\Parameter('columns', 'programId'),
+            new \Oara\Curl\Parameter('columns', 'timeOfVisit'),
+            new \Oara\Curl\Parameter('columns', 'timeOfEvent'),
+            new \Oara\Curl\Parameter('columns', 'timeInSession'),
+            new \Oara\Curl\Parameter('columns', 'lastModified'),
+            new \Oara\Curl\Parameter('columns', 'epi1'),
+            new \Oara\Curl\Parameter('columns', 'eventName'),
+            new \Oara\Curl\Parameter('columns', 'pendingStatus'),
+            new \Oara\Curl\Parameter('columns', 'siteName'),
+            new \Oara\Curl\Parameter('columns', 'graphicalElementName'),
+            new \Oara\Curl\Parameter('columns', 'graphicalElementId'),
+            new \Oara\Curl\Parameter('columns', 'productName'),
+            new \Oara\Curl\Parameter('columns', 'productNrOf'),
+            new \Oara\Curl\Parameter('columns', 'productValue'),
+            new \Oara\Curl\Parameter('columns', 'affiliateCommission'),
+            new \Oara\Curl\Parameter('columns', 'link'),
+            new \Oara\Curl\Parameter('columns', 'leadNR'),
+            new \Oara\Curl\Parameter('columns', 'orderNR'),
+            new \Oara\Curl\Parameter('columns', 'pendingReason'),
+            new \Oara\Curl\Parameter('columns', 'orderValue'),
+            new \Oara\Curl\Parameter('columns', 'eventId'),           // Added <PN> 2017-07-03
+            new \Oara\Curl\Parameter('isPostBack', ''),
+            new \Oara\Curl\Parameter('metric1.lastOperator', '/'),
+            new \Oara\Curl\Parameter('interval', ''),
+            new \Oara\Curl\Parameter('favoriteDescription', ''),
+            new \Oara\Curl\Parameter('event_id', '0'),
+            new \Oara\Curl\Parameter('pending_status', '1'),
+            new \Oara\Curl\Parameter('run_as_organization_id', ''),
+            new \Oara\Curl\Parameter('minRelativeIntervalStartTime', '0'),
+            new \Oara\Curl\Parameter('includeWarningColumn', 'true'),
+            new \Oara\Curl\Parameter('metric1.summaryType', 'NONE'),
+            new \Oara\Curl\Parameter('metric1.operator1', '/'),
+            new \Oara\Curl\Parameter('latestDayToExecute', '0'),
+            new \Oara\Curl\Parameter('showAdvanced', 'true'),
+            new \Oara\Curl\Parameter('currencyId', $currency),              // Added <PN> 2018-03-26
+            new \Oara\Curl\Parameter('breakdownOption', '1'),
+            new \Oara\Curl\Parameter('metric1.midFactor', ''),
+            new \Oara\Curl\Parameter('reportTitleTextKey', 'REPORT3_SERVICE_REPORTS_AAFFILIATEEVENTBREAKDOWNREPORT_TITLE'),
+            new \Oara\Curl\Parameter('setColumns', 'true'),
+            new \Oara\Curl\Parameter('metric1.columnName1', 'orderValue'),
+            new \Oara\Curl\Parameter('metric1.columnName2', 'orderValue'),
+            new \Oara\Curl\Parameter('reportPrograms', ''),
+            new \Oara\Curl\Parameter('metric1.midOperator', '/'),
+            new \Oara\Curl\Parameter('dateSelectionType', $byDateUpdated ? '2' : '1'),    // Retrieve transactions by Date Event (default = 1) or by Date Last Updated (2)
+            new \Oara\Curl\Parameter('favoriteName', ''),
+            new \Oara\Curl\Parameter('dateType', '1'),
+            new \Oara\Curl\Parameter('period', 'custom_period'),
+            new \Oara\Curl\Parameter('tabMenuName', ''),
+            new \Oara\Curl\Parameter('maxIntervalSize', '0'),
+            new \Oara\Curl\Parameter('favoriteId', ''),
+            new \Oara\Curl\Parameter('sortBy', 'timeOfEvent'),
+            new \Oara\Curl\Parameter('metric1.name', ''),
+            new \Oara\Curl\Parameter('customKeyMetricCount', '0'),
+            new \Oara\Curl\Parameter('metric1.factor', ''),
+            new \Oara\Curl\Parameter('showFavorite', 'false'),
+            new \Oara\Curl\Parameter('separator', ','),
+            new \Oara\Curl\Parameter('format', 'CSV')
+        );
+        $valuesFormExport[] = new \Oara\Curl\Parameter('startDate', self::formatDate($dStartDate));
+        $valuesFormExport[] = new \Oara\Curl\Parameter('endDate', self::formatDate($dEndDate));
+        if (!empty($this->_credentials['idSite'])){
+            $valuesFormExport[] = new \Oara\Curl\Parameter('affiliateId', $this->_credentials['idSite']);
+        }
+        else{
+            $valuesFormExport[] = new \Oara\Curl\Parameter('affiliateId', '');
+        }
+        $urls = array();
+        $urls[] = new \Oara\Curl\Request('http://publisher.tradedoubler.com/pan/aReport3Internal.action?', $valuesFormExport);
+        $exportReport = $this->_client->get($urls);
+
+        $exportReport[0] = self::checkReportError($exportReport[0], $urls[0]);
+        $exportData = \str_getcsv($exportReport[0], "\r\n");
+        $num = \count($exportData);
+        for ($i = 2; $i < $num - 1; $i++) {
+
+            try {
+                $transactionExportArray = \str_getcsv($exportData[$i], ",");
+
+                if (!isset($transactionExportArray[2])) {
+                    throw new \Exception('Problem getting transaction\n\n');
+                }
+                if (\count($this->_sitesAllowed) == 0 || \in_array($transactionExportArray[13], $this->_sitesAllowed)) {
+
+                    if (!empty(trim($transactionExportArray[7] . $transactionExportArray[8]))) {
+                        // Skip rows without a unique_id
+                        $transaction = Array();
+                        $transaction['merchantId'] = $transactionExportArray[2];
+                        $transactionDate = self::toDate(\substr($transactionExportArray[4], 0, -6));
+                        if ($transactionDate === false) {
+                            // Bad date ... skip
+                            $transaction['date'] = '';
+                        }
+                        else {
+                            $transaction['date'] = $transactionDate->format("Y-m-d H:i:s");
+                        }
+                        if ($transactionExportArray[8] != '') {
+                            $transaction['unique_id'] = \substr($transactionExportArray[8], 0, 200);
+                        } else
+                            if ($transactionExportArray[7] != '') {
+                                $transaction['unique_id'] = \substr($transactionExportArray[7], 0, 200);
+                            } else {
+                                throw new \Exception("No Identifier");
+                            }
+
+
+                        if ($transactionExportArray[9] != '') {
+                            $transaction['custom_id'] = $transactionExportArray[9];
+                        }
+
+                        $transaction['status'] = \Oara\Utilities::STATUS_PENDING;
+                        if ($transactionExportArray[12] == 'A') {
+                            $transaction['status'] = \Oara\Utilities::STATUS_CONFIRMED;
+                        } else
+                            if ($transactionExportArray[12] == 'P') {
+                                $transaction['status'] = \Oara\Utilities::STATUS_PENDING;
+                            } else
+                                if ($transactionExportArray[12] == 'D') {
+                                    $transaction['status'] = \Oara\Utilities::STATUS_DECLINED;
+                                }
+
+                        if ($transactionExportArray[20] != '') {
+                            $transaction['amount'] = \Oara\Utilities::parseDouble($transactionExportArray[20]);
+                        } else {
+                            $transaction['amount'] = \Oara\Utilities::parseDouble($transactionExportArray[21]);
+                        }
+
+                        $transaction['commission'] = \Oara\Utilities::parseDouble($transactionExportArray[21]);
+                        $transaction['event_id'] = $transactionExportArray[11];
+                        $transaction['currency'] = $currency;
+                        $transactions[] = $transaction;
+                    }
+                }
+            }
+            catch (\Exception $e) {
+                echo PHP_EOL."TradeDoublerEx - getTransactionList err: ".$e->getMessage().PHP_EOL;
+            }
+        }
+        return $transactions;
+    }
+}
