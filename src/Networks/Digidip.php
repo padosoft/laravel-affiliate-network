9f37122b206e55b674efdbdd480867caaebe2ad3
jessica-capuozzo <jessica.capuozzo@bravosconto.it>
2022-01-12 10:41:39 +0100

    digidip - added merchants list

--- src/Networks/Digidip.php
+++ src/Networks/Digidip.php
@@ -70,79 +70,88 @@ class Digidip extends AbstractNetwork implements NetworkInterface
     }
 
     /**
-	 * @return array of Merchants
-	 * @throws \Exception
-	 */
+     * @return array of Merchants
+     * @throws \Exception
+     */
     public function getMerchants() : array
     {
-	    throw new \Exception("Not implemented yet");
+        $arrResult = array();
+        $merchantList = $this->_network->getMerchantList();
+        foreach ($merchantList as $merchant) {
+            $Merchant = Merchant::createInstance();
+            $Merchant->merchant_ID = $merchant['cid'];
+            $Merchant->name = $merchant['name'];
+            $Merchant->url = $merchant['url'];
+            $arrResult[] = $Merchant;
+        }
+        return $arrResult;
     }
 
     /**
-	 * @param null $merchantID
-	 * @param int $page
-	 * @param int $items_per_page
-	 * @return DealsResultset  array of Deal
-	 * @throws \Exception
-	 */
+     * @param null $merchantID
+     * @param int $page
+     * @param int $items_per_page
+     * @return DealsResultset  array of Deal
+     * @throws \Exception
+     */
     public function getDeals($merchantID=NULL,int $page=0,int $items_per_page=100 ): DealsResultset
     {
-	    throw new \Exception("Not implemented yet");
+        throw new \Exception("Not implemented yet");
 
     }
 
-   	/**
-	 * @param \DateTime $dateFrom
-	 * @param \DateTime $dateTo
-	 * @param array $arrMerchantID
-	 * @return array of Transaction
-	 * @throws \Exception
-	 */
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param array $arrMerchantID
+     * @return array of Transaction
+     * @throws \Exception
+     */
     public function getSales(\DateTime $dateFrom, \DateTime $dateTo, array $arrMerchantID = array()) : array
     {
-	    $arrResult = array();
+        $arrResult = array();
         try{
-	        $transactionList = $this->_network->getTransactionList($arrMerchantID, $dateFrom, $dateTo);
-	        foreach($transactionList as $transaction) {
-		        $Transaction = Transaction::createInstance();
-		        if (isset($transaction['currency']) && !empty($transaction['currency'])) {
-			        $Transaction->currency = $transaction['currency'];
-		        } else {
-			        $Transaction->currency = "EUR";
-		        }
-		        $Transaction->status = $transaction['status'];
-		        $Transaction->amount = $transaction['amount'];
-		        array_key_exists_safe( $transaction,'custom_id' ) ? $Transaction->custom_ID = $transaction['custom_id'] : $Transaction->custom_ID = '';
-		        $Transaction->title = '';
-		        $Transaction->unique_ID = $transaction['unique_id'];
-		        $Transaction->commission = $transaction['commission'];
-		        $date = new \DateTime($transaction['date']);
-		        $Transaction->date = $date;
-		        // Future use - Only few providers returns these dates values - <PN> - 2017-06-29
-		        if (isset($transaction['click_date']) && !empty($transaction['click_date'])) {
-			        $Transaction->click_date = new \DateTime($transaction['click_date']);
-		        }
-		        if (isset($transaction['update_date']) && !empty($transaction['update_date'])) {
-			        $Transaction->update_date = new \DateTime($transaction['update_date']);
-		        }
-		        $Transaction->merchant_ID = $transaction['merchantId'];
-		        $Transaction->campaign_name =  $transaction['merchantName'];
-		        $Transaction->IP =  $transaction['IP'];
-		        $Transaction->approved = false;
-		        if ($Transaction->status == \Oara\Utilities::STATUS_CONFIRMED){
-			        $Transaction->approved = true;
-		        }
-		        $arrResult[] = $Transaction;
-	        }
+            $transactionList = $this->_network->getTransactionList($arrMerchantID, $dateFrom, $dateTo);
+            foreach($transactionList as $transaction) {
+                $Transaction = Transaction::createInstance();
+                if (isset($transaction['currency']) && !empty($transaction['currency'])) {
+                    $Transaction->currency = $transaction['currency'];
+                } else {
+                    $Transaction->currency = "EUR";
+                }
+                $Transaction->status = $transaction['status'];
+                $Transaction->amount = $transaction['amount'];
+                array_key_exists_safe( $transaction,'custom_id' ) ? $Transaction->custom_ID = $transaction['custom_id'] : $Transaction->custom_ID = '';
+                $Transaction->title = '';
+                $Transaction->unique_ID = $transaction['unique_id'];
+                $Transaction->commission = $transaction['commission'];
+                $date = new \DateTime($transaction['date']);
+                $Transaction->date = $date;
+                // Future use - Only few providers returns these dates values - <PN> - 2017-06-29
+                if (isset($transaction['click_date']) && !empty($transaction['click_date'])) {
+                    $Transaction->click_date = new \DateTime($transaction['click_date']);
+                }
+                if (isset($transaction['update_date']) && !empty($transaction['update_date'])) {
+                    $Transaction->update_date = new \DateTime($transaction['update_date']);
+                }
+                $Transaction->merchant_ID = $transaction['merchantId'];
+                $Transaction->campaign_name =  $transaction['merchantName'];
+                $Transaction->IP =  $transaction['IP'];
+                $Transaction->approved = false;
+                if ($Transaction->status == \Oara\Utilities::STATUS_CONFIRMED){
+                    $Transaction->approved = true;
+                }
+                $arrResult[] = $Transaction;
+            }
 
         }
         catch (\Exception $e){
-	        echo "[Digidip][Error] " . $e->getMessage() . PHP_EOL;
-	        var_dump($e->getTraceAsString());
-	        throw new \Exception($e);
+            echo "[Digidip][Error] " . $e->getMessage() . PHP_EOL;
+            var_dump($e->getTraceAsString());
+            throw new \Exception($e);
         }
 
-	    return $arrResult;
+        return $arrResult;
 
     }
 
@@ -173,10 +182,10 @@ class Digidip extends AbstractNetwork implements NetworkInterface
 
 
     /**
-	 * @param array $params
-	 * @return ProductsResultset
-	 * @throws \Exception
-	 */
+     * @param array $params
+     * @return ProductsResultset
+     * @throws \Exception
+     */
     public function getProducts(array $params = []): ProductsResultset
     {
         // TODO: Implement getProducts() method.
