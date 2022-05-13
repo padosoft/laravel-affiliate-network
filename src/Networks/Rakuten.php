25f7ea43f550bb3032de5ed3d73f71420107cfa4
jessica-capuozzo <jessica.capuozzo@bravosconto.it>
2022-05-13 16:21:24 +0200

    added rakuten (linkshare migration); composer require padosoft/php-oara:2.7.07

---
+++ src/Networks/Rakuten.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace Padosoft\AffiliateNetwork\Networks;
+
+use Padosoft\AffiliateNetwork\Transaction;
+use Padosoft\AffiliateNetwork\DealsResultset;
+use Padosoft\AffiliateNetwork\Merchant;
+use Padosoft\AffiliateNetwork\Stat;
+use Padosoft\AffiliateNetwork\Deal;
+use Padosoft\AffiliateNetwork\AbstractNetwork;
+use Padosoft\AffiliateNetwork\NetworkInterface;
+use Padosoft\AffiliateNetwork\ProductsResultset;
+
+/**
+ * Class Rakuten
+ * @package Padosoft\AffiliateNetwork\Networks
+ */
+class Rakuten extends AbstractNetwork implements NetworkInterface
+{
+    /**
+     * @var object
+     */
+    private $_network = null;
+    private $_username = '';
+    private $_password = '';
+    private $_apiClient = null;
+    protected $_tracking_parameter    = 'u1';
+    private $_idSite = '';
+
+    /**
+     * @method __construct
+     */
+    public function __construct(string $username, string $password, string $idSite = '')
+    {
+        $this->_network = new \Oara\Network\Publisher\Rakuten();
+        $this->_username = $username;
+        $this->_password = $password;
+        $this->_idSite = $idSite;
+
+        $this->login( $this->_username, $this->_password, $idSite );
+    }
+
+    public function login(string $username, string $password,string $idSite=''): bool{
+        $this->_logged = false;
+        if (isNullOrEmpty( $username ) || isNullOrEmpty( $password )) {
+
+            return false;
+        }
+        $this->_username = $username;
+        $this->_password = $password;
+        $credentials = array();
+        $credentials["user"] = $this->_username;
+        $credentials["password"] = $this->_password;
+        $credentials["idSite"] = $idSite;
+        $this->_network->login($credentials);
+        if ($this->_network->checkConnection()) {
+            $this->_logged = true;
+        }
+
+        return $this->_logged;
+    }
+
+    /**
+     * @return bool
+     */
+    public function checkLogin() : bool
+    {
+        return $this->_logged;
+    }
+
+    /**
+     * @return array of Merchants
+     */
+    public function getMerchants() : array
+    {
+        $arrResult = array();
+        $merchantList = $this->_network->getMerchantList();
+        foreach($merchantList as $merchant) {
+            $Merchant = Merchant::createInstance();
+            $Merchant->merchant_ID = $merchant['cid'];
+            $Merchant->name = $merchant['name'];
+            $Merchant->status = $merchant['status'];
+            $Merchant->termination_date = $merchant['termination_date'];
+            $Merchant->url = $merchant['url'];
+            $arrResult[] = $Merchant;
+        }
+        return $arrResult;
+    }
+
+    /**
+     * @param int $merchantID
+     * @return array of Deal
+     */
+    public function getDeals($merchantID=NULL,int $page=0,int $items_per_page=10 ): DealsResultset
+    {
+        $arrResult = array();
+
+        $network = $_ENV['RAKUTEN_NETWORK'];
+        $result = DealsResultset::createInstance();
+
+        $arrVouchers = $this->_network->getVouchers($network);
+
+        foreach($arrVouchers as $voucher) {
+            if (!empty($voucher['tracking']) && !empty($voucher['advertiser_id'])) {
+                $Deal = Deal::createInstance();
+                $Deal->deal_ID = md5($voucher['tracking']);    // Use link to generate a unique deal ID
+                $Deal->merchant_ID = $voucher['advertiser_id'];
+                $Deal->code = $voucher['code'];
+                $Deal->description = $voucher['description'] . ' ' . $voucher['restriction'];
+                $Deal->start_date = $Deal->convertDate($voucher['start_date']);
+                $Deal->start_date->setTime(0, 0, 0);
+                $Deal->end_date = $Deal->convertDate($voucher['end_date']);
+                $Deal->end_date->setTime(23, 59, 59);
+                $Deal->default_track_uri = $voucher['tracking'];
+                $Deal->is_exclusive = false;
+                $Deal->deal_type = $voucher['type'];
+                $arrResult[] = $Deal;
+            }
+        }
+
+        $result->deals[]=$arrResult;
+
+        return $result;
+        ;
+    }
+
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $merchantID
+     * @return array of Transaction
+     */
+    public function getSales(\DateTime $dateFrom, \DateTime $dateTo, array $arrMerchantID = array()) : array
+    {
+
+        $arrResult = array();
+        $this->_network->_nid = $_ENV['RAKUTEN_NETWORK'];
+        $transactionList = $this->_network->getTransactionList($arrMerchantID, $dateFrom, $dateTo);
+        foreach($transactionList as $transaction) {
+            $Transaction = Transaction::createInstance();
+            if (isset($transaction['currency']) && !empty($transaction['currency'])) {
+                $Transaction->currency = $transaction['currency'];
+            } else {
+                $Transaction->currency = "EUR";
+            }
+            $Transaction->status = $transaction['status'];
+            $Transaction->amount = $transaction['amount'];
+            array_key_exists_safe( $transaction,'custom_id' ) ? $Transaction->custom_ID = $transaction['custom_id'] : $Transaction->custom_ID = '';
+            $Transaction->title = '';
+            $Transaction->unique_ID = $transaction['unique_id'];
+            $Transaction->commission = $transaction['commission'];
+            $date = new \DateTime($transaction['date']);
+            $Transaction->date = $date; // $date->format('Y-m-d H:i:s');
+            // Future use - Only few providers returns these dates values - <PN> - 2017-06-29
+            if (isset($transaction['click_date']) && !empty($transaction['click_date'])) {
+                $Transaction->click_date = new \DateTime($transaction['click_date']);
+            }
+            if (isset($transaction['update_date']) && !empty($transaction['update_date'])) {
+                $Transaction->update_date = new \DateTime($transaction['update_date']);
+            }
+            $Transaction->merchant_ID = $transaction['merchantId'];
+            $Transaction->campaign_name =  $transaction['merchantName'];
+            $Transaction->IP =  $transaction['IP'];
+            $Transaction->approved = false;
+            if ($Transaction->status==\Oara\Utilities::STATUS_CONFIRMED){
+                $Transaction->approved = true;
+            }
+            $arrResult[] = $Transaction;
+        }
+
+        return $arrResult;
+    }
+
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $merchantID
+     * @return array of Stat
+     */
+    public function getStats(\DateTime $dateFrom, \DateTime $dateTo, int $merchantID = 0) : array
+    {
+        // TODO
+        return array();
+        /*
+        $this->_apiClient->setConnectId($this->_username);
+        $this->_apiClient->setSecretKey($this->_password);
+        $dateFromIsoEngFormat = $dateFrom->format('Y-m-d');
+        $dateToIsoEngFormat = $dateTo->format('Y-m-d');
+        $response = $this->_apiClient->getReportBasic($dateFromIsoEngFormat, $dateToIsoEngFormat);
+        $arrResponse = json_decode($response, true);
+        $reportItems = $arrResponse['reportItems'];
+        $Stat = Stat::createInstance();
+        $Stat->reportItems = $reportItems;
+
+        return array($Stat);
+        */
+    }
+
+
+    /**
+     * @param  array $params
+     *
+     * @return ProductsResultset
+     */
+    public function getProducts(array $params = []): ProductsResultset
+    {
+        // TODO: Implement getProducts() method.
+        throw new \Exception("Not implemented yet");
+    }
+
+    public function getTrackingParameter(){
+        return $this->_tracking_parameter;
+    }
+}
